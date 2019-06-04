c27bb156287a2bad5d5d642a8bc771b77784063b
Yijun Tay <yijun.tay@arup.com>
2019-06-04 23:49:29 +0800

    created feature layer

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 require([
   "esri/Map",
   "esri/views/MapView",
-  "esri/layers/GraphicsLayer",
-  "esri/Graphic",
+  "esri/layers/FeatureLayer",
   "esri/widgets/Home",
   "esri/widgets/Locate"
 ],
 
   function (
     Map, MapView,
-    GraphicsLayer, Graphic,
+    FeatureLayer,
     Home, Locate) {
 
     var map = new Map({
@@ -42,54 +41,129 @@ require([
 
     fetch(requestUrl)
       .then(response => response.json())
-      .then(jsonResponse => createGraphicsLayer(jsonResponse[1]))
+      .then(jsonResponse => createFeatures(jsonResponse[1]))
+      .then(features => createFeatureLayer(features))
+      .then(featureLayer => map.add(featureLayer))
       .catch(error => console.error('Error:', error));
 
-    // Iterate through array to generate point graphics layer
-    createGraphicsLayer = function (input) {
+    // Iterate through JSON response to create array of features
+    createFeatures = function (input) {
 
-      var layer = new GraphicsLayer;
+      var features = [];
 
-      // Iterate through array to generate points
       for (i = 0; i < input.length; i++) {
+      // for (i = 0; i < 5; i++) {
 
-        // Create point geometry
-        var point = {
+        var feature = {};
+
+        // Create feature geomtry
+        feature.geometry = {
           type: "point",
           longitude: input[i].lng,
           latitude: input[i].lat
         };
 
-        // Create a symbol for drawing the point
-        var markerSymbol = {
-          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-          color: [0, 157, 59],
-        };
+        // Create feature attributes
+        feature.attributes = input[i];
 
-        var popupTemplate = {
-          title: "{mex_trading_name}",
-          content:
-            "<p><b>Address:</b> <br>{address}</p>" +
-            "<p><a href='{facebook}' target='_blank'>Facebook</a>" +
-            "<br><a href='{instagram}' target='_blank'>Instagram</a>" +
-            "<br><a href='{website}' target='_blank'>Website</a></p>"
+        // Conditional value from JSON
+        // To-do: write this in a more efficient way
+        if (feature.attributes.type_fnb == "1") {
+          feature.attributes.service = "fnb"
+        }
+        else if (feature.attributes.type_retail == "1") {
+          feature.attributes.service = "retail"
+        }
+        else if (feature.attributes.type_service == "1") {
+          feature.attributes.service = "service"
+        }
+        else {
+          feature.attributes.service = "others"
         }
 
-        // Create a graphic and add the geometry and symbol to it
-        var pointGraphic = new Graphic({
-          geometry: point,
-          symbol: markerSymbol,
-          attributes: input[i],
-          popupTemplate: popupTemplate
-        });
-
-        // Add graphic to layer
-        layer.graphics.add(pointGraphic);
-
+        features.push(feature);
       }
 
-      // Add layer to map
-      map.add(layer);
+      return features
+
+    };
+
+    // Create feature layer from features array
+    createFeatureLayer = function (features) {
+
+      var renderer = {
+        type: "simple",  // autocasts as new SimpleRenderer()
+        symbol: {
+          type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+          size: 6,
+          color: [0, 157, 59, 0.8],
+          outline: {  // autocasts as new SimpleLineSymbol()
+            width: 0.5,
+            color: "white"
+          }
+        }
+      };
+
+      var popupTemplate = {
+        title: "{mex_trading_name}",
+        content:
+          "<p><b>Address:</b> <br>{address}</p>" +
+          "<p><a href='{facebook}' target='_blank'>Facebook</a>" +
+          "<br><a href='{instagram}' target='_blank'>Instagram</a>" +
+          "<br><a href='{website}' target='_blank'>Website</a></p>"
+      };
+
+      var fields = [
+        {
+          name: "id",
+          alias: "id",
+          type: "oid"
+        },
+        {
+          name: "mex_trading_name",
+          alias: "mex_trading_name",
+          type: "string"
+        },
+        {
+          name: "address",
+          alias: "address",
+          type: "string"
+        },
+        {
+          name: "service",
+          alias: "service",
+          type: "string"
+        },
+        {
+          name: "facebook",
+          alias: "facebook",
+          type: "string"
+        },
+        {
+          name: "instagram",
+          alias: "instagram",
+          type: "string"
+        },
+        {
+          name: "website",
+          alias: "website",
+          type: "string"
+        },
+      ];
+
+      var featureLayer = new FeatureLayer({
+
+        // create an instance of esri/layers/support/Field for each field object
+        fields: fields,
+        objectIdField: "id",
+        geometryType: "point",
+        spatialReference: { wkid: 4326 },
+        source: features,
+        popupTemplate: popupTemplate,
+        renderer: renderer,
+      });
+
+      return featureLayer
 
     };
 
